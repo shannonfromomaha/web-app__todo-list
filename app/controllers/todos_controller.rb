41db885ad2a834f3e767ea336439aa3e872c9e97
Shannon Jackson <shannonfromomaha@gmail.com>
2016-02-24 13:44:45 -0600

    working on to do list for to do list

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,6 +3,23 @@ MyApp.get "/" do
   erb :"/todos/welcome"
 end
 
+MyApp.get "/todos/new" do
+  binding.pry
+  @current_user = User.find_by_id(session["user_id"])
+  @items
+  erb :"/todos/new"
+end
+
+MyApp.post "/newitem" do
+  t = Todo.new
+  t.title = params[:title]
+  t.description = params[:description]
+  t.completed = false
+  t.user_id = params[:user_id]
+  t.save
+  erb :"/todos/new"
+end
+
 #DB.define_table("todos")
 #DB.define_column("todos", "title", "string")
 #DB.define_column("todos", "description", "text")
