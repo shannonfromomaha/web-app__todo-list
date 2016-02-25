100f7d1f11fa9b86562dd0100f884f66fec85d3d
Shannon Jackson <shannonfromomaha@gmail.com>
2016-02-25 12:54:59 -0600

    1245

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -10,39 +10,47 @@ MyApp.before "/todos*" do
   end
 end
 
+MyApp.get "/todos/welcome" do
+  @items = Todo.all
+
 MyApp.get "/todos/new" do
+  #as it stands, 
+  #this would be more of a "personal profile" page
   @items = Todo.where("user_id" => @currentuser.id)
   erb :"/todos/new"
 end
 
-MyApp.post "todos/newitem" do
+MyApp.post "/todos/newitem" do
   t = Todo.new
   t.title = params[:title]
   t.description = params[:description]
   t.completed = false
   t.user_id = params[:user_id]
+  #needs assignment id today
+  #needs a category id today
   t.save
   redirect "/todos/new"
 end
 
-MyApp.post "/updatelist" do
+MyApp.post "/todos/update" do
+  #this marks a thing as done or not done
   array = params[:items]
   Todo.update(array)
   redirect "/todos/new"  
 end
 
-MyApp.get "/todos/delete" do
+#these two are messy now bc you might not want to delete--
+#just reassign or rename a task
+MyApp.get "/todos/edit" do
   @items = Todo.where("user_id" => @currentuser.id)
   erb :"/todos/delete"
 end
-
-MyApp.post "/deleteitems" do
+#so post todos/delete should actually be ON edit page
+MyApp.post "/todos/delete" do
   array = params[:items]
   Todo.delete(array)
   redirect "/todos/new"
 end
-
-
 
 #DB.define_table("todos")
 #DB.define_column("todos", "title", "string")
