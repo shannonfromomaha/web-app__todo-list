c9a0a541b9c0e287ee70cea1e9d91cc4ce766442
Shannon Jackson <shannonfromomaha@gmail.com>
2016-02-28 13:06:19 -0600

    added assignments table

---
+++ app/models/assignment.rb
@@ -0,0 +1,6 @@
+class Assignment < ActiveRecord::Base
+end
+
+#DB.define_table("assignments")
+#DB.define_column("assignments","todo_id","integer")
+#DB.define_column("assignments","user_id","integer")
